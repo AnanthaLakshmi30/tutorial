28ee5806b0deb271ba2319b9a90256a335157060
AnanthaLakshmi30 <108463092+AnanthaLakshmi30@users.noreply.github.com>
2024-04-13 11:00:34 +0530

    first commit for testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component ,OnInit} from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'sampleForm';
+export class AppComponent implements OnInit{
+sampleForm!:FormGroup;
+
+constructor(private fb:FormBuilder){
+
 }
+
+ngOnInit(){
+  this.formIntilization()
+}
+
+formIntilization(){
+  this.sampleForm=this.fb.group({
+    'firstName':[''],
+    'lastName':['']
+
+  })
+}
+
+submit(){
+  const data={
+    FirstName:this.sampleForm.value['firstName'],
+    LastName:this.sampleForm.value['lastName'],
+  }
+  console.log(data)
+}
+
+  
+  }
+
+
+
